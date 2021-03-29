64545b175ecadfeaf41d1ba7b84f4a054ebd6940
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-03-29 11:38:10 +0200

    update slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -7,20 +7,30 @@ const Carousel = () => {
 
     const [sliderImg, setSliderImg] = useState(1);
 
+    const slideLeft = () => {
+        sliderImg > 0 && setSliderImg(sliderImg - 1)
+    }
+
+    const slideRight = () => {
+        sliderImg < sliderImages.length -1 && setSliderImg(sliderImg + 1)
+    }
+
+
     return (
         <div className="slider">
             <div 
                 className="slider__inner" 
                 style={{backgroundImage: `url(${sliderImages[sliderImg].img})`}}>
-                    <div className="slider__left" onClick={()=>{sliderImg > 0 && setSliderImg(sliderImg - 1)}}>
-                        <i className="fas fa-arrow-left"></i>
+                    <div className="slider__side" onClick={slideLeft}>
+                        <i className="fas fa-long-arrow-alt-left"></i>
                     </div>
                     <div className="slider__center">
                         <h2>{sliderImages[sliderImg].title}</h2>
                         <h3>{sliderImages[sliderImg].subtitle}</h3>
+                        <p>Przemek Rura | 5cm za aparatem</p>
                     </div>
-                    <div className="slider__right" onClick={()=>{sliderImg < sliderImages.length -1 && setSliderImg(sliderImg + 1)}}>
-                        <i className="fas fa-arrow-right"></i>
+                    <div className="slider__side" onClick={slideRight}>
+                        <i className="fas fa-long-arrow-alt-right"></i>
                     </div>
             </div>
         </div>
