b40fddb3e51c1a9ade062cac18b594ceb167cbba
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-03-30 16:22:06 +0200

    add hamburger menu

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 
@@ -6,6 +6,15 @@ import { Link } from "react-router-dom";
 import './../../css/Navbar.css';
 
 const Navbar = () =>{
+
+    const [navbar, setNavbar] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setNavbar(!navbar);
+    }
+    const closeMobileMenu = () => {
+        setNavbar(false);
+    }
 
     const navItems = [
         {name: "Strona Główna", link:"/"},
@@ -16,21 +25,28 @@ const Navbar = () =>{
 
     const nav = navItems.map(item => (
         <li key={item.name}>
-            <Link to={item.link}>
+            <Link className="navbar__link" to={item.link} onClick={closeMobileMenu}>
                 {item.name}
             </Link>
         </li>
     ))
 
     return (
-            <div className="header">
-                <p>test</p>
-                <nav>
-                    <ul>
-                        {nav}
-                    </ul>
-                </nav>
+        <>
+            <div className="navbar">
+                <div className="navbar__left">
+                    <Link to="/" className="navbar__logo"></Link>
+                </div>
+                <div className="navbar__right">
+                    <nav>
+                        <ul className="navbar__links" id={navbar ? "hidden" : ""}>
+                            {nav}
+                        </ul>
+                    </nav>
+                    <i onClick={toggleMobileMenu} className="fas fa-camera navbar__icon"></i>
+                </div>
             </div>
+        </>
 
     )
 }
